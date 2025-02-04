45175efc4ce98c9cec830e661705206d1c9e4b73
RaimondsPolis <149487123+RaimondsPolis@users.noreply.github.com>
2025-02-04 22:51:10 +0200

    mostly done

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/kontakti')
+def kontakti():
+    return render_template('kontakti.html')
+
+@app.route('/projekti')
+def projekti():
+    return render_template('projekti.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
